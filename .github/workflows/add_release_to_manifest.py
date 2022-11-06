ecd6f28f3b0f7df806e4110807e343574d7fa47c
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2022-11-06 17:11:19 +0000

    fixed issue with checksum during release

--- .github/workflows/add_release_to_manifest.py
+++ .github/workflows/add_release_to_manifest.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+import hashlib
 import json
 import sys
 
 if len(sys.argv) != 3:
-    print("ERROR: Wrong arguments!\nUsage: add_release_to_manifest.py <version> <checksum>")
+    print("ERROR: Wrong arguments!\nUsage: add_release_to_manifest.py <version> <file>")
     sys.exit(1)
 
 with open("manifest.json", "r") as f:
@@ -11,9 +12,14 @@ with open("manifest.json", "r") as f:
 
 version = sys.argv[1]
 
+with open(sys.argv[2], "r") as f:
+    checksum = hashlib.md5()
+    while chunk := f.read(4096):
+        checksum.update(chunk)
+
 new_version_info = {
     "version": version,
-    "checksum": sys.argv[2],
+    "checksum": checksum.hexdigest(),
     "sourceUrl": f"https://github.com/infinityofspace/jellyfin-alexa-plugin/releases/download/{version}/AlexaSkill_{version}.zip",
     "changelog": "See for more details: https://github.com/infinityofspace/jellyfin-alexa-plugin/releases",
     "targetAbi": "10.8.0.0",
