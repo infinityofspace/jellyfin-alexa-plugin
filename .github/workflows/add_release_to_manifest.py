e007d770e6dd05334c97d8601a87ad2b6242e8c3
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2022-11-06 17:06:08 +0000

    fixed release workflow issue

--- .github/workflows/add_release_to_manifest.py
+++ .github/workflows/add_release_to_manifest.py
@@ -2,17 +2,19 @@ from datetime import datetime
 import json
 import sys
 
-if len(sys.argv) != 4:
-    print("ERROR: Wrong arguments!\nUsage: add_release_to_manifest.py <version> <checksum> <url>")
+if len(sys.argv) != 3:
+    print("ERROR: Wrong arguments!\nUsage: add_release_to_manifest.py <version> <checksum>")
     sys.exit(1)
 
 with open("manifest.json", "r") as f:
     manifest = json.load(f)
 
+version = sys.argv[1]
+
 new_version_info = {
-    "version": sys.argv[1],
+    "version": version,
     "checksum": sys.argv[2],
-    "sourceUrl": sys.argv[3],
+    "sourceUrl": f"https://github.com/infinityofspace/jellyfin-alexa-plugin/releases/download/{version}/AlexaSkill_{version}.zip",
     "changelog": "See for more details: https://github.com/infinityofspace/jellyfin-alexa-plugin/releases",
     "targetAbi": "10.8.0.0",
     "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
